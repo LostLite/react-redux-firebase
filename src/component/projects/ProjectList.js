1fb8ebccc53840a44209ab91e1c9b6e08b464594
KE-William <ke-william@KE-Williams-MacBook-Pro.local>
2019-02-08 13:07:50 +0300

    Loaded the dashboard component with project lists component and notification component

---
+++ src/component/projects/ProjectList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const ProjectList = (props) =>{
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>Posted by MarioPlan</p>
+                    <p className="grey-text">3rd Sept, 2am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>Posted by MarioPlan</p>
+                    <p className="grey-text">3rd Sept, 2am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>Posted by MarioPlan</p>
+                    <p className="grey-text">3rd Sept, 2am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">
+                        Project Title
+                    </span>
+                    <p>Posted by MarioPlan</p>
+                    <p className="grey-text">3rd Sept, 2am</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
